de9df5c25bac677e78226f3354c48fd17a59a3fe
Wesley Leandro <wesleyleandro.dev@gmail.com>
2021-10-18 18:08:28 -0300

    Refactored register client

--- app/src/controllers/clients-controlles/AuthController.ts
+++ app/src/controllers/clients-controlles/AuthController.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm'
 import { Request, Response } from 'express'
+import axios from 'axios'
 import * as bcrypt from 'bcrypt'
 
 import { Client } from '../../models/Client'
@@ -11,6 +12,7 @@ import { CPFValidate } from '../../utils/CPFValidate'
 import { generateRandomNumber } from '../../utils/RandomValueGenerate'
 import { sendMail } from '../../utils/SendMail'
 import { generateToken } from '../../config/JWT'
+import { State } from '../../models/State'
 
 type loginTypes = {
     cpf: string
@@ -21,17 +23,34 @@ type loginTypes = {
 type userDataTypes = {
     id: string
     fullName: string
-    birthDate: string
-    phone: string
-    city: string
     cpf: string
+    birthDate: string
     email: string
+    phone: string
+    zipCode: string
     password: string
 }
 
 type confirmationEmailTypes = {
     id: string
     confirmation: string
+}
+
+type data = {
+    cep: string
+    logradouro: string
+    complemento: string
+    bairro: string
+    localidade: string
+    uf: string
+    ibge: string
+    gia: string
+    ddd: string
+    siafi: string
+}
+
+type apiCorreiosResponseType = {
+    data: data
 }
 
 export const signIn = async (request: Request, response: Response) => {
@@ -78,66 +97,87 @@ export const signIn = async (request: Request, response: Response) => {
 
 export const registerNewClient = async (request: Request, response: Response) => {
     try {
-        const userData: userDataTypes = request.body.userData
+        const {
+            fullName,
+            cpf,
+            birthDate,
+            email,
+            phone,
+            zipCode,
+            password
+        }: userDataTypes = request.body.userData
 
-        if (
-            !userData.fullName ||
-            !userData.birthDate ||
-            !userData.phone ||
-            !userData.cpf ||
-            !userData.email ||
-            !userData.password
-        ) {
+        if (!fullName || !birthDate || !phone || !zipCode || !email || !password) {
             return response.status(401).json({ message: 'Dados incompletos' })
         }
 
-        if (!CPFValidate(userData.cpf)) {
+        if (!CPFValidate(cpf)) {
             return response.status(400).json({ message: 'CPF inválido' })
         }
 
-        return response.status(200).json({ msg: 'Is Running' })
+        const client = await getRepository(Client).findOne({ where: { cpf } })
 
-        // const client = await getRepository(Client).findOne({
-        //     where: {
-        //         cpf: userData.cpf
-        //     }
-        // })
+        if (client) {
+            return response.status(302).json({ message: 'CPF já cadastrado' })
+        }
 
-        // if (client) {
-        //     if (!client.emailConfirmated) {
-        //         return response.status(426).json({ message: 'Confirmação de email pendente' })
-        //     }
+        const city = await getRepository(City).findOne({
+            where: {
+                zipCode
+            }
+        })
 
-        //     return response.status(302).json({ message: 'CPF já cadastrado' })
-        // }
+        if (city) {
+            var address = await getRepository(ClientAddress).save({
+                city,
+                street: '',
+                district: '',
+            })
+        } else {
+            var {
+                data: {
+                    bairro,
+                    cep,
+                    localidade,
+                    logradouro,
+                    uf
+                }
+            }: apiCorreiosResponseType = await axios.get(`https://viacep.com.br/ws/${zipCode}/json/`)
 
-        // const city = await getRepository(City).findOne({
-        //     where: {
-        //         name: userData.city
-        //     }
-        // })
+            const state = await getRepository(State).findOne({
+                where: {
+                    initials: uf
+                }
+            })
 
-        // const address = await getRepository(ClientAddress).save({
-        //     street: '',
-        //     city
-        // })
+            const newCity = await getRepository(City).save({
+                name: localidade,
+                zipCode: cep,
+                state
+            })
 
-        // const passwordEncrypted = bcrypt.hashSync(userData.password, 10)
+            var newAddress = await getRepository(ClientAddress).save({
+                city: newCity,
+                street: logradouro,
+                district: bairro,
+            })
+        }
+
+        const passwordEncrypted = bcrypt.hashSync(password, 10)
         // const MIN = 100000
         // const MAX = 999999
 
         // const confirmEmail = `TB-${generateRandomNumber(MIN, MAX)}`
 
-        // const newClient = await getRepository(Client).save({
-        //     full_name: userData.fullName,
-        //     birth_date: convertDate(userData.birthDate),
-        //     phone: userData.phone,
-        //     cpf: userData.cpf,
-        //     email: userData.email,
-        //     address,
-        //     signature: confirmEmail,
-        //     password: passwordEncrypted
-        // })
+        const newClient = await getRepository(Client).save({
+            fullName: fullName,
+            birthDate: convertDate(birthDate),
+            phone: phone,
+            cpf: cpf,
+            email: email,
+            address: address ? address : newAddress,
+            password: passwordEncrypted
+        })
 
         // if (newClient) {
         //     const message = `Olá ${newClient.full_name}, já estamos quase lá! Para completar o seu cadastro, digite as seguintes credenciais ${confirmEmail}`
@@ -150,7 +190,11 @@ export const registerNewClient = async (request: Request, response: Response) =>
         //     return response.status(201).json({ id: newClient.id })
         // }
 
-        // return response.status(400).json({ message: 'Houve um erro' })
+        if (newClient) {
+            return response.status(400).json({ token: 'TOKEN_DE_ACESSO' })
+        }
+
+        return response.status(400).json({ message: 'Houve um erro' })
     } catch (error) {
         return response.status(400).json(error)
     }
