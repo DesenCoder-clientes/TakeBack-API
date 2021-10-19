36f7197ef0cb215fdaf81b91c8545917f1d98427
Wesley Leandro <wesleyleandro.dev@gmail.com>
2021-10-19 20:31:41 -0300

    configured ssl

--- ormconfig.js
+++ ormconfig.js
@@ -2,6 +2,9 @@ module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
   "logging": false,
+  "dialectOptions": {
+    "ssl": true
+  },
   "entities": [process.env.ENTITIES_DIR],
   "migrations": [process.env.MIGRATIONS_DIR],
   "subscribers": [process.env.SUBSCRIBERS_DIR],
