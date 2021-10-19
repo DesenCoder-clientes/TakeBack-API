4f8841e2a39443a7a9683e9dcb573814031150fa
Wesley Leandro <wesleyleandro.dev@gmail.com>
2021-10-19 08:44:47 -0300

    Implemented sign in and sign up of the clients

--- app/src/controllers/clients-controlles/AuthController.ts
+++ app/src/controllers/clients-controlles/AuthController.ts
@@ -9,8 +9,6 @@ import { City } from '../../models/City'
 
 import { convertDate } from '../../utils/ConvertDateToISOString'
 import { CPFValidate } from '../../utils/CPFValidate'
-import { generateRandomNumber } from '../../utils/RandomValueGenerate'
-import { sendMail } from '../../utils/SendMail'
 import { generateToken } from '../../config/JWT'
 import { State } from '../../models/State'
 
@@ -29,11 +27,6 @@ type userDataTypes = {
     phone: string
     zipCode: string
     password: string
-}
-
-type confirmationEmailTypes = {
-    id: string
-    confirmation: string
 }
 
 type data = {
@@ -77,17 +70,12 @@ export const signIn = async (request: Request, response: Response) => {
             return response.status(401).json({ message: 'Senha incorreta' })
         }
 
-        const PAYLOAD = {
+        const token = generateToken({
             id: client.id,
-            name: client.fullName,
-            email: client.email,
+            fullName: client.fullName,
             cpf: client.cpf,
-            phone: client.phone
-        }
-
-        const EXPIRES_IN = 60 * 60 * 24
-
-        const token = generateToken(PAYLOAD, process.env.JWT_CONFIG_AUTH_USER, EXPIRES_IN)
+            email: client.email
+        }, process.env.JWT_PRIVATE_KEY, process.env.JWT_EXPIRES_IN)
 
         return response.status(200).json({ ACCESS_TOKEN: token })
     } catch (error) {
@@ -137,12 +125,15 @@ export const registerNewClient = async (request: Request, response: Response) =>
             var {
                 data: {
                     bairro,
-                    cep,
                     localidade,
                     logradouro,
                     uf
                 }
             }: apiCorreiosResponseType = await axios.get(`https://viacep.com.br/ws/${zipCode}/json/`)
+
+            if (!uf) {
+                return response.status(404).json({ message: 'Cep não localizado' })
+            }
 
             const state = await getRepository(State).findOne({
                 where: {
@@ -152,7 +143,7 @@ export const registerNewClient = async (request: Request, response: Response) =>
 
             const newCity = await getRepository(City).save({
                 name: localidade,
-                zipCode: cep,
+                zipCode,
                 state
             })
 
@@ -164,10 +155,6 @@ export const registerNewClient = async (request: Request, response: Response) =>
         }
 
         const passwordEncrypted = bcrypt.hashSync(password, 10)
-        // const MIN = 100000
-        // const MAX = 999999
-
-        // const confirmEmail = `TB-${generateRandomNumber(MIN, MAX)}`
 
         const newClient = await getRepository(Client).save({
             fullName: fullName,
@@ -179,54 +166,18 @@ export const registerNewClient = async (request: Request, response: Response) =>
             password: passwordEncrypted
         })
 
-        // if (newClient) {
-        //     const message = `Olá ${newClient.full_name}, já estamos quase lá! Para completar o seu cadastro, digite as seguintes credenciais ${confirmEmail}`
-            
-        //     sendMail(
-        //         newClient.email,
-        //         'Take Back - Confirmação de email',
-        //         message
-        //     )
-        //     return response.status(201).json({ id: newClient.id })
-        // }
-
         if (newClient) {
-            return response.status(400).json({ token: 'TOKEN_DE_ACESSO' })
+            const token = generateToken({
+                id: newClient.id,
+                fullName: newClient.fullName,
+                cpf: newClient.cpf,
+                email: newClient.email
+            }, process.env.JWT_PRIVATE_KEY, process.env.JWT_EXPIRES_IN)
+
+            return response.status(400).json({ ACCESS_TOKEN: token })
         }
 
         return response.status(400).json({ message: 'Houve um erro' })
-    } catch (error) {
-        return response.status(400).json(error)
-    }
-}
-
-export const confirmationEmail = async (request: Request, response: Response) => {
-    try {
-        const { id, confirmation }: confirmationEmailTypes = request.body
-
-        const client = await getRepository(Client).findOne(id)
-
-        if (!client) {
-            return response.status(404).json({ message: 'Usuário não encontrado' })
-        }
-
-        if (client.signature === confirmation) {
-            const PAYLOAD = {
-                id: client.id,
-                name: client.fullName,
-                email: client.email,
-                cpf: client.cpf,
-                phone: client.phone
-            }
-
-            const EXPIRES_IN = 60 * 60 * 24
-
-            const token = generateToken(PAYLOAD, process.env.JWT_CONFIG_AUTH_USER, EXPIRES_IN)
-
-            return response.status(200).json({ ACCESS_TOKEN: token })
-        }
-
-        return response.status(401).json({ message: 'Confirmação inválida' })
     } catch (error) {
         return response.status(400).json(error)
     }
