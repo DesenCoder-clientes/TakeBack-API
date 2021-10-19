488eda15d0c49812c8f78c22494bacf6413c5ff0
Wesley Leandro <wesleyleandro.dev@gmail.com>
2021-10-19 20:43:10 -0300

    Reconfigured environment variables

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,12 @@
 module.exports = {
   "type": "postgres",
-  "url": process.env.DATABASE_URL,
+  // "url": process.env.DATABASE_URL,
+  "user": process.env.DB_USER,
+  "password": process.env.DB_PASS,
+  "database": process.env.DB_NAME,
+  "host": process.env.DB_HOST,
+  "port": process.env.DB_PORT,
+  "ssl": true,
   "logging": false,
   "dialectOptions": {
     "ssl": true
