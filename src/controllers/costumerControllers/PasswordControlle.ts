57c5909876db94d365e4d570722da5e06082f1c7
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2021-11-01 15:31:18 -0300

    Implemented update password

---
+++ src/controllers/costumerControllers/PasswordControlle.ts
@@ -0,0 +1,46 @@
+import { getRepository } from "typeorm";
+import { Request, Response } from "express";
+import * as bcrypt from "bcrypt";
+
+import { Consumers } from "../../models/Consumer";
+
+type ConsumerRequestToUpdatePassword = {
+  password: string;
+  newPassword: string;
+};
+
+export const updatePassword = async (request: Request, response: Response) => {
+  try {
+    const consumerID = request["tokenPayload"].id;
+    const { password, newPassword }: ConsumerRequestToUpdatePassword =
+      request.body;
+
+    if (!password || !newPassword) {
+      return response.status(400).json({ message: "Dados não informados" });
+    }
+
+    const consumerData = await getRepository(Consumers).findOne(consumerID, {
+      select: ["password"],
+    });
+
+    const passwordMatch = await bcrypt.compare(password, consumerData.password);
+
+    if (!passwordMatch) {
+      return response.status(401).json({ message: "Senha incorreta" });
+    }
+
+    const newPasswordEncrypted = bcrypt.hashSync(newPassword, 10);
+
+    const { affected } = await getRepository(Consumers).update(consumerID, {
+      password: newPasswordEncrypted,
+    });
+
+    if (affected === 1) {
+      return response.status(200).json({ message: "Senha alterada" });
+    }
+
+    return response.status(417).json({ message: "Houve um erro" });
+  } catch (error) {
+    return response.status(500).json(error);
+  }
+};
--- src/routes/ConsumerRoutes.ts
+++ src/routes/ConsumerRoutes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import * as Auth from "../controllers/costumerControllers/AuthController";
 import * as Data from "../controllers/costumerControllers/AppDataController";
 import * as Update from "../controllers/costumerControllers/UpdateController";
+import * as Pass from "../controllers/costumerControllers/PasswordControlle";
 
 import { AuthMiddleware } from "../middlewares/costumerMiddlewares/AuthMiddleware";
 
@@ -18,5 +19,6 @@ routes.put("/update-data", Update.updateData);
 routes.put("/update-phone", Update.updatePhone);
 routes.put("/update-email", Update.updateEmail);
 routes.put("/update-address", Update.updateAddress);
+routes.put("/update-password", Pass.updatePassword);
 
 export default routes;
