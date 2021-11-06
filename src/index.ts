ad5118b4f8c3cc008063e1aedcb0000c2ed6900e
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2021-11-06 13:36:15 -0300

    Complete account data implemented

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 import "reflect-metadata";
 import * as express from "express";
+import { Request, Response, NextFunction } from "express";
 import * as cors from "cors";
 import "./database";
 
@@ -23,5 +24,14 @@ app.use("/support", SupportRoutes);
 app.use("/consumer", ConsumerRoutes);
 app.use("/company", CompanyRoutes);
 app.use("/admin", AdminRoutes);
+
+app.use((request: Request, response: Response, next: NextFunction) => {
+  response.status(404).json({ message: "Endpoint inexistente" });
+  next();
+});
+
+app.use((request: Request, response: Response) => {
+  return response.status(500).json({ message: "Erro inexperado" });
+});
 
 app.listen(process.env.PORT || 3333);
