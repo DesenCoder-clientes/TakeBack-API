7fe6a083eefb94493a4138c2a4ec9d15b37a19dc
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2022-03-02 14:19:15 -0300

    Fixed error on send mails

--- src/controllers/manager/managerPaymentOrder/SendTicketToEmailUseCase.ts
+++ src/controllers/manager/managerPaymentOrder/SendTicketToEmailUseCase.ts
@@ -1,21 +1,59 @@
+import { getRepository } from "typeorm";
 import { InternalError } from "../../../config/GenerateErros";
 import transporter from "../../../config/SMTP";
+import { Companies } from "../../../models/Company";
+import { PaymentOrder } from "../../../models/PaymentOrder";
+import { PaymentOrderStatus } from "../../../models/PaymentOrderStatus";
 
 interface Props {
+  paymentOrderId: number;
   fileName: string;
   filePath: string;
-  useCustomEmail: boolean;
+  useCustomEmail: string;
   customEmail: string;
 }
 
 class SendTicketToEmailUseCase {
-  async execute({ fileName, filePath, useCustomEmail, customEmail }: Props) {
+  async execute({
+    paymentOrderId,
+    fileName,
+    filePath,
+    useCustomEmail,
+    customEmail,
+  }: Props) {
+    if (!paymentOrderId || !fileName || !filePath) {
+      throw new InternalError("Boleto não informado", 400);
+    }
+
+    if (useCustomEmail === "true" && !customEmail) {
+      throw new InternalError("Email não informado", 400);
+    }
+
+    const paymentOrder = await getRepository(PaymentOrder).findOne({
+      where: { id: paymentOrderId },
+      relations: ["company"],
+    });
+
+    if (!paymentOrder) {
+      throw new InternalError("Ordem de pagamento não localizada", 404);
+    }
+
+    const company = await getRepository(Companies).findOne({
+      where: { id: paymentOrder.company.id },
+    });
+
+    if (!company) {
+      throw new InternalError("Empresa não localizada", 404);
+    }
+
+    const message = ``;
+
     transporter.sendMail(
       {
         from: process.env.MAIL_CONFIG_USER,
-        to: customEmail,
-        subject: "Boleto",
-        text: "Segue o seu boleto para o pagamento dos cashbacks",
+        to: useCustomEmail === "true" ? customEmail : company.email,
+        subject: "Boleto TakeBack",
+        text: "",
         attachments: [
           {
             filename: fileName,
@@ -32,7 +70,27 @@ class SendTicketToEmailUseCase {
       }
     );
 
-    return "Sucesso";
+    const paymentOrderStatus = await getRepository(PaymentOrderStatus).findOne({
+      where: { description: "Aguardando pagamento" },
+    });
+
+    const updatedPaymentOrder = await getRepository(PaymentOrder).update(
+      paymentOrderId,
+      {
+        ticketName: fileName,
+        ticketPath: filePath,
+        status: paymentOrderStatus,
+      }
+    );
+
+    if (updatedPaymentOrder.affected === 0) {
+      throw new InternalError(
+        "Ouve um erro ao atualizar o status da ordem de pagemento",
+        400
+      );
+    }
+
+    return "Email enviado";
   }
 }
 
