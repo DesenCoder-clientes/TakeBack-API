62674fdfc7bc03111ca830715c6e4cea7d9c8b14
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2022-03-06 11:44:32 -0300

    Updated query to find cashback details

--- src/controllers/costumer/costumerCashBack/CostumerFindCashbackDetailsUseCase.ts
+++ src/controllers/costumer/costumerCashBack/CostumerFindCashbackDetailsUseCase.ts
@@ -15,56 +15,44 @@ interface FindAppProps {
 class CostumerFindCashbackDetailsUseCase {
   async execute({ transactionID }: FindAppProps) {
     const transactions = await getRepository(Transactions)
-      .createQueryBuilder("t")
+      .createQueryBuilder("transaction")
       .select([
-        "t.id",
-        "t.totalAmount",
-        "t.cashbackPercent",
-        "t.cashbackAmount",
-        "t.cancellationDescription",
-        "t.createdAt",
+        "transaction.id",
+        "transaction.totalAmount",
+        "transaction.amountPayWithOthersMethods",
+        "transaction.amountPayWithTakebackBalance",
+        "transaction.cashbackPercent",
+        "transaction.cashbackAmount",
+        "transaction.cancellationDescription",
+        "transaction.createdAt",
       ])
       .addSelect([
-        "c.fantasyName",
-        "ca.street",
-        "ca.district",
-        "ca.number",
+        "company.fantasyName",
+        "address.street",
+        "address.district",
+        "address.number",
         "city.name",
-        "ts.id",
-        "ts.description",
-        // "tt.isUp",
+        "status.id",
+        "status.description",
       ])
-      .leftJoin(Companies, "c", "c.id = t.companies")
-      .leftJoin(CompaniesAddress, "ca", "ca.id = c.address")
-      .leftJoin(City, "city", "city.id = ca.city")
-      .leftJoin(TransactionStatus, "ts", "ts.id = t.transactionStatus")
-      // .leftJoin(TransactionTypes, "tt", "tt.id = t.transactionTypes")
-      .where("t.id = :transactionID", { transactionID })
+      .leftJoin(Companies, "company", "company.id = transaction.companies")
+      .leftJoin(CompaniesAddress, "address", "address.id = company.address")
+      .leftJoin(City, "city", "city.id = address.city")
+      .leftJoin(TransactionStatus, "status", "status.id = transaction.transactionStatus")
+      .where("transaction.id = :transactionID", { transactionID })
       .getRawOne();
 
     const paymentMethods = await getRepository(TransactionPaymentMethods)
-      .createQueryBuilder("tpm")
-      .select(["tpm.id", "tpm.cashbackPercentage", "tpm.cashbackValue"])
-      .addSelect(["pm.id", "pm.description"])
-      .leftJoin(CompanyPaymentMethods, "cpm", "cpm.id = tpm.paymentMethod")
-      .leftJoin(PaymentMethods, "pm", "pm.id = cpm.paymentMethod")
-      .leftJoin(Transactions, "t", "t.id = tpm.transactions")
-      .where("t.id = :transactionID", { transactionID })
+      .createQueryBuilder("transactionPaymentMethod")
+      .select(["transactionPaymentMethod.id", "transactionPaymentMethod.cashbackPercentage", "transactionPaymentMethod.cashbackValue"])
+      .addSelect(["paymentMethod.id", "paymentMethod.description"])
+      .leftJoin(CompanyPaymentMethods, "companyPaymentMethod", "companyPaymentMethod.id = transactionPaymentMethod.paymentMethod")
+      .leftJoin(PaymentMethods, "paymentMethod", "paymentMethod.id = companyPaymentMethod.paymentMethod")
+      .leftJoin(Transactions, "transaction", "transaction.id = transactionPaymentMethod.transactions")
+      .where("transaction.id = :transactionID", { transactionID })
       .getRawMany();
 
-    let valuePayNotWithBalance = 0;
-    paymentMethods.map((item) => {
-      if (item.pm_id !== 1) {
-        valuePayNotWithBalance =
-          valuePayNotWithBalance +
-          item.tpm_cashbackValue / item.tpm_cashbackPercentage;
-      }
-    });
-
-    const valuePayWithBalance =
-      transactions.t_totalAmount - valuePayNotWithBalance;
-
-    return { transactions, paymentMethods, valuePayWithBalance };
+    return { transactions, paymentMethods };
   }
 }
 
