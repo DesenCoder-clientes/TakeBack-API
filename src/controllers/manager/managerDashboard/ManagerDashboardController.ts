ed509e71ae253b5150a8c5b472a32aaca0770af3
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2022-03-15 15:46:32 -0300

    Created report cashbacks by payment method

--- src/controllers/manager/managerDashboard/ManagerDashboardController.ts
+++ src/controllers/manager/managerDashboard/ManagerDashboardController.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
 import { TotalizerReportUseCase } from "./TotalizerReportUseCase";
+import { CashbacksByPaymentMethodReportUseCase } from "./CashbacksByPaymentMethodReportUseCase";
 
 class DashboardController {
   async dashboardReport(request: Request, response: Response) {
     const totalizersReport = new TotalizerReportUseCase();
+    const transactionsReport = new CashbacksByPaymentMethodReportUseCase();
 
     const totalizers = await totalizersReport.execute();
+    const transactions = await transactionsReport.execute();
 
-    return response.status(200).json({ totalizers });
+    return response.status(200).json({ transactions });
   }
 }
 
