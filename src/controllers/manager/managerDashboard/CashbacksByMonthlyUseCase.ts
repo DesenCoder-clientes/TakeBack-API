ef1983325e5ccbd86af092ff093b896341bedce7
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2022-03-15 16:10:40 -0300

    Created report cashbacks per month

---
+++ src/controllers/manager/managerDashboard/CashbacksByMonthlyUseCase.ts
@@ -0,0 +1,43 @@
+import { getRepository } from "typeorm";
+import { CompanyPaymentMethods } from "../../../models/CompanyPaymentMethod";
+import { PaymentMethods } from "../../../models/PaymentMethod";
+import { Transactions } from "../../../models/Transaction";
+import { TransactionPaymentMethods } from "../../../models/TransactionPaymentMethod";
+import { TransactionStatus } from "../../../models/TransactionStatus";
+
+class CashbacksByMonthlyUseCase {
+  async execute() {
+    const transactions = await getRepository(TransactionPaymentMethods)
+      .createQueryBuilder("paymentMethod")
+      .select("SUM(paymentMethod.cashbackValue)", "valor")
+      .addSelect("DATE_TRUNC('month', transaction.createdAt)", "datada")
+      .leftJoin(
+        Transactions,
+        "transaction",
+        "transaction.id = paymentMethod.transactions"
+      )
+      .leftJoin(
+        TransactionStatus,
+        "status",
+        "status.id = transaction.transactionStatus"
+      )
+      .leftJoin(
+        CompanyPaymentMethods,
+        "companyPaymentMethod",
+        "companyPaymentMethod.id = paymentMethod.paymentMethod"
+      )
+      .leftJoin(
+        PaymentMethods,
+        "method",
+        "method.id = companyPaymentMethod.paymentMethod"
+      )
+      .where("status.description = :status", { status: "Aprovada" })
+      .andWhere("method.isTakebackMethod = :isTakeback", { isTakeback: false })
+      .groupBy("DATE_TRUNC('month', transaction.createdAt)")
+      .getRawMany();
+
+    return transactions;
+  }
+}
+
+export { CashbacksByMonthlyUseCase };
--- src/controllers/manager/managerDashboard/ManagerDashboardController.ts
+++ src/controllers/manager/managerDashboard/ManagerDashboardController.ts
@@ -1,16 +1,19 @@
 import { Request, Response } from "express";
 import { TotalizerReportUseCase } from "./TotalizerReportUseCase";
 import { CashbacksByPaymentMethodReportUseCase } from "./CashbacksByPaymentMethodReportUseCase";
+import { CashbacksByMonthlyUseCase } from "./CashbacksByMonthlyUseCase";
 
 class DashboardController {
   async dashboardReport(request: Request, response: Response) {
     const totalizersReport = new TotalizerReportUseCase();
     const transactionsReport = new CashbacksByPaymentMethodReportUseCase();
+    const cashbackMonthly = new CashbacksByMonthlyUseCase();
 
     const totalizers = await totalizersReport.execute();
     const transactions = await transactionsReport.execute();
+    const cashbacksMonthly = await cashbackMonthly.execute();
 
-    return response.status(200).json({ transactions });
+    return response.status(200).json({ cashbacksMonthly });
   }
 }
 
