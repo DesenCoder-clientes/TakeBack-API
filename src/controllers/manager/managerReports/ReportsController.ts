603e860e7f81d70536f3ec046f478f33caaa35cb
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2022-03-16 15:45:47 -0300

    Implemented route to find filter options

--- src/controllers/manager/managerReports/ReportsController.ts
+++ src/controllers/manager/managerReports/ReportsController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { PaymentOrderReportUseCase } from "./PaymentOrderReportUseCase";
 import { MonthlyPaymentReportUseCase } from "./MonthlyPaymentReportUseCase";
+import { FindFiltersOptionsUseCase } from "./FindFiltersOptionsUseCase";
 
 class ReportsController {
   async paymentOrderReport(request: Request, response: Response) {
@@ -21,6 +22,14 @@ class ReportsController {
     const report = await generateReport.execute({ filters });
 
     return response.status(200).json(report);
+  }
+
+  async findFilterOptions(request: Request, response: Response) {
+    const findFilters = new FindFiltersOptionsUseCase();
+
+    const filters = await findFilters.execute();
+
+    return response.status(200).json(filters);
   }
 }
 
